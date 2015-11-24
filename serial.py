cffb11ef05f1f89673733cd9df9d65e16c73c7f4
TuanTrain <antuantran@college.harvard.edu>
2015-11-24 00:31:02 -0500

    added dilate code
    returns list of tuples on the border of image

--- serial.py
+++ serial.py
@@ -1,6 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def dilate(image):
+    '''
+    TODO
+    '''
+    dilatedList = []
+    dilated = np.copy(image)
+    (h,w) = np.shape(image)
+    for i in range(h):
+        for j in range(w):
+            if image[i,j] == 1:
+                if i > 0:
+                    dilated[i - 1, j] = 1
+                if i < h - 1:
+                    dilated[i + 1, j] = 1
+                if j > 0:
+                    dilated[i, j - 1] = 1
+                if j < w - 1:
+                    dilated[i, j + 1] = 1
+    for i in range(h):
+        for j in range(w):
+            if image[i,j] == 0 and dilated[i,j] == 1:
+                dilatedList.append((i,j))
+    return dilatedList
+
+
 if __name__ == '__main__':
 
     '''
@@ -27,16 +52,13 @@ if __name__ == '__main__':
     mask = np.zeros((h,w), dtype=float)
     mask[top:top + imheight, left:left + imwidth] = 1
 
-    # dilated pixes
+    
+
 
     plt.imshow(mask)
     plt.show()
 
 
-def dilate(image):
-    '''
-    TODO
-    '''
 
 def sum_square_diff(template, mask, image):
     '''
@@ -44,3 +66,9 @@ def sum_square_diff(template, mask, image):
     '''
 
 
+# test for dilate
+#mask[top + 25:top + imheight - 25, left + 25:left + imwidth - 25] = 0
+    #test = np.copy(mask)
+    #plt.imshow(test, cmap = 'Greys', interpolation = 'none')
+    #print dilate(test)
+    
