7b2f4348389e33a99a656533821c870ac1418c15
Evan Yao <evanyao@Evans-MacBook-Pro.local>
2015-11-24 00:33:05 -0500

    combining with Tuantuan

--- serial.py
+++ serial.py
@@ -1,8 +1,93 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter
+import sys
+
+def dilate(image):
+    '''
+    TODO
+    '''
+    pass
+
+def find_match(template, mask, image):
+    '''
+    Finds a match of the template in the mask
+    '''
+    # checking that the size of the template and mask are the same
+    assert np.shape(template) == np.shape(mask)
+
+    # size variables
+    (h_t, w_t) = np.shape(template)
+    (h_i, w_i) = np.shape(image)
+
+    results = []
+    # looping through all possible windows
+    for h in range(h_i - h_t):
+        for w in range(w_i - w_t):
+            results.append(sum_square_error(template, image[], mask[]), image[h,w])
+
+    return results
+
+
+def sum_square_error(template, image_chunk, mask):
+    global gaussian
+
+    '''
+    Returns the sum of square error of a particular template on
+    top of an image chunk. Uses the provided gaussian for weighting.
+    '''
+    # checking sizes
+    assert np.shape(template) == np.shape(image_chunk)
+    assert np.shape(image_chunk) == np.shape(gaussian)
+    assert np.shape(mask) == np.shape(image_chunk)
+
+    total = 0
+
+    for i in range(np.shape(template)[0]):
+        for j in range(np.shape(template)[1]):
+            if mask[i,j]:
+                total += (template[i,j] - image_chunk[i,j]) ** 2) * gaussian[i,j]
+
+    return total
+
+
+def test_square_diff():
+    pass
+
+
+def gkern(kernlen=21, nsig=3):
+    """Returns a 2D Gaussian kernel array."""
+
+    interval = (2*nsig+1.)/(kernlen)
+    x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
+    kern1d = np.diff(st.norm.cdf(x))
+    kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
+    kernel = kernel_raw/kernel_raw.sum()
+    return kernel
+
 
 if __name__ == '__main__':
+    '''
+    Playing, Testing Code
+    '''
 
+    test_image = np.zeros((11, 11))
+    test_template = np.zeros((5,5))
+
+    test_image[0:11:2,0:11:2] = 1
+    test_image[1:11:2,1:11:2] = 1
+
+    test_template[0:5:2, :] = 1
+    test_template[:, 1:11:2] = 1
+
+    plt.imshow(test_template, cmap='Greys', interpolation='none')
+    plt.show()
+
+    plt.imshow(test_image, cmap = 'Greys', interpolation='none')
+    plt.show()
+
+    if len(sys.argv) != 1:
+        sys.exit(0)
     '''
     Pre-processing: loading image, setting up template
     '''
@@ -10,6 +95,8 @@ if __name__ == '__main__':
     image = plt.imread('images/sand_template.gif')
 
     (h, w) = (300, 300)
+
+    window_size = 5
 
     (imheight, imwidth, color) = np.shape(image)
 
@@ -28,19 +115,5 @@ if __name__ == '__main__':
     mask[top:top + imheight, left:left + imwidth] = 1
 
     # dilated pixes
-
     plt.imshow(mask)
     plt.show()
-
-
-def dilate(image):
-    '''
-    TODO
-    '''
-
-def sum_square_diff(template, mask, image):
-    '''
-    TODO
-    '''
-
-
